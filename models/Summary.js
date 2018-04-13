ec1b8712145eb372d69266e88398305166511afb
astie086 <astie086@gmail.com>
2018-04-12 22:16:44 -0700

    update

---
+++ models/Summary.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+
+var SummarySchema = new Schema({
+  body: String
+});
+
+var Summary = mongoose.model("Summary", SummarySchema);
+
+module.exports = Summary;
